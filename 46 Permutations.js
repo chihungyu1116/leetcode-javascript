0e13e2dda120a424614393f0e9bf06c7ffa82366
ignacio chiazzo cardarello <ignaciochiazzo@gmail.com>
2017-02-17 19:17:26 -0300

    Permutation Solution

--- 46 Permutations.js
+++ 46 Permutations.js
@@ -62,6 +62,28 @@ var generate = function(nums, index, visited, output, result) {
 }
 
 
+// Another clear solution
+var permute = function(nums) {
+    return permuteAux(nums, []);
+};
+
+var permuteAux = function(nums, partialNums) {
+  if(nums === null || nums.length === 0) {
+      return [partialNums];
+  }
+  var listArrays = [];
+  for(var i = 0; i < nums.length; i++) {
+      var withoutI = nums.slice(0,i).concat(nums.slice(i + 1, nums.length));
+      var partial = partialNums.concat([nums[i]]);
+      var sol = permuteAux(withoutI, partial);
+      if(sol.legnth !== 0) {
+        listArrays = listArrays.concat(sol);
+      }
+  }
+  return listArrays;
+};
+
+
 
 
 
