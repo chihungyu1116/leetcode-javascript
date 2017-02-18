efdff0e2fa4c05a0302196ed3dd9e05490dc0cfe
ignacio chiazzo cardarello <ignaciochiazzo@gmail.com>
2017-02-18 10:14:55 -0300

    Added another solution to Permutation ||
    Similar approach that permutation.js

--- 47 Permutations II.js
+++ 47 Permutations II.js
@@ -51,3 +51,31 @@ var generatePermute = function(nums, step, currentResult, visited, finalResult) 
         }
     }
 }
+
+//Another Solution, similar approach that Permutation.js
+var permuteUnique = function(nums) {
+    return permut(nums.sort(), []);
+};
+
+var permut = function(nums, partial) {
+    if(nums.length === 0) {
+        return [partial];
+    }
+    var listSol = [];
+    for(var i = 0; i < nums.length; i++) {
+        var endRepeated = i;
+        while(endRepeated < nums.length && nums[i] === nums[endRepeated]) {
+            endRepeated++;
+        }
+        
+        var arrayWithoutI = nums.slice(0,i).concat(nums.slice(i + 1, nums.length));
+        var partialSol = partial.concat([nums[i]]);
+        var sol = permut(arrayWithoutI, partialSol);
+        if(sol.length > 0){
+            listSol = listSol.concat(sol);
+        }
+        i = endRepeated - 1;
+    }
+    return listSol;
+};
+
