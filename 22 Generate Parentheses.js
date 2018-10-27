df55c95fe1d50f4f57998aa1d7305baf5efdaa67
Ignacio Chiazzo Cardarello <ignaciochiazzo@gmail.com>
2018-10-27 16:16:16 -0400

    Update 22 Generate Parentheses.js

--- 22 Generate Parentheses.js
+++ 22 Generate Parentheses.js
@@ -23,3 +23,21 @@ var generate = function(result, output, depth, n, left, right){
        generate(result,output + ')',depth + 1, n, left, right+1);
    }
 }
+
+var genParAuxApproach2 = function(str, leftPar, rightPar, index, totalCharCount, sol) {
+  if(index == totalCharCount) {
+    if(rightPar == leftPar) {
+      sol.push(str);
+    }
+    return;
+  }
+   var strLeft = insertAt(str, index, "(");
+  genParAuxApproach2(strLeft, leftPar + 1, rightPar, index + 1, totalCharCount, sol);
+   if(rightPar == leftPar) { return; }
+  
+  var strRight = insertAt(str, index, ")");
+  genParAuxApproach2(strRight, leftPar, rightPar + 1, index + 1, totalCharCount, sol);
+}
+ var insertAt = function(str, position, value) {
+  return str.slice(0, position) + value + str.slice(position);
+}
